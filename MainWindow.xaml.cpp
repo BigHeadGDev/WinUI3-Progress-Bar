5605e403be3b01e5c87f8e07c693a1a41218ce19
1 <jihoon-lim>
2022-11-22 17:17:00 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::App8::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+        auto windowNative{ this->try_as<::IWindowNative>() };
+        HWND hWnd{ 0 };
+        windowNative->get_WindowHandle(&hWnd);
+        SetWindowPos(hWnd, NULL, 200, 200, 390, 290, NULL);
+        this->Title(L"프로그래스 바 실습");
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    
+}
